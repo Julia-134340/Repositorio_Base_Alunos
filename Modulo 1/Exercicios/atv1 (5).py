12ca5d07e38348a86498925055bffdd45b8d1b73
swkjulia <juliaoliveira2810@gmail.com>
2025-09-10 14:11:22 -0300

    Delete Modulo 1/Exercicios/atv1 (5).py

--- Modulo 1/Exercicios/atv1 (5).py
+++
@@ -1,7 +0,0 @@
-def soma(a, b):
-    return a + b
-n1 = int(input("digite o primeiro numero: "))
-n2 = int(input("digite o segundo numero: "))
-
-resultado = soma(n1, n2)
-print("a soma dos numeros é: ", resultado)
